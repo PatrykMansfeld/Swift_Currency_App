8f4b84d4cdb8a8871be2794620151fcb2c841f8a
Patryk_Mansfeld <patryk.mansfeld@gmail.com>
2024-04-14 13:08:01 +0200

    Basic_Swift_Structure

---
+++ CurrencyCheck/CurrencyCheck/CurrencyCheckApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyCheckApp.swift
+//  CurrencyCheck
+//
+//  Created by Patryk Mansfeld on 14/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyCheckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
